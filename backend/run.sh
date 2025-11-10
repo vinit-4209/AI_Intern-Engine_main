6dd19e253df467a85acd0f00029d0054308e147a
vinit-4209 <vinitkumar4209@gmail.com>
2025-11-10 14:12:52 +0530

    3rd commit

--- backend/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Script to run the FastAPI backend server
-
-echo "Starting PM Internship Allocation Backend..."
-echo "Installing dependencies..."
-pip install -r requirements.txt
-
-echo "Starting server on http://localhost:8000"
-uvicorn main:app --reload --host 0.0.0.0 --port 8000
