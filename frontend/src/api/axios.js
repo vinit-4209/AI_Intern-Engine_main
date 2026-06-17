76f2eb722b32777d6ecb3cf377a58e8047a17ab1
vinit-4209 <vinitkumar4209@gmail.com>
2026-06-17 13:38:36 +0530

    Fix production API URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,15 @@
+// import axios from "axios"
+
+// const api = axios.create({
+//   baseURL: "http://localhost:8000/api",
+// })
+
+// export default api
+
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: import.meta.env.VITE_API_URL,
 })
 
 export default api
